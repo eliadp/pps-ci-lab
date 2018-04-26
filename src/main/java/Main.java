e363d595160f15ac5c882d9cacad1c1932de5d11
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2018-04-26 10:28:00 +0200

    Updated .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+/**
+ * App main class
+ */
 public class Main {
 
+    /**
+     * Empty main
+     *
+     * @param args ignored
+     */
     public static void main(final String args[]) {
 
     }
